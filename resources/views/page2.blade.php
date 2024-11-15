274836f4e30c08e87ca669820a7a7c29a10ef99d
omartins-zs <gabrielmatheus209@gmail.com>
2024-11-15 17:35:38 -0300

    Adicionando Layout nas Views

--- resources/views/page2.blade.php
+++ resources/views/page2.blade.php
@@ -3,6 +3,8 @@
 @section('content')
     <h1>Welcome View and Blade</h1>
 
+    <h2>Page 2</h2>
+
     <h3> the value is: {{ $value }} </h3>
     {{-- <h3> the value is: <?= $value ?></h3> --}}
     {{-- <h3> the value is: <?php echo $value; ?> </h3> --}}
