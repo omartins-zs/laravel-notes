de5d1cc65fcf9c0eb7c0f3a2495d073f8204a41b
omartins-zs <gabrielmatheus209@gmail.com>
2024-11-15 17:35:02 -0300

    Criando Function e Routes page2 e page3

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -9,7 +9,23 @@ class MainController extends Controller
     //
     public function index($value)
     {
-        return view('main',['value' => $value]);
+        return view('main', ['value' => $value]);
+
+        // return view('main')->with('value',$value);
+        // return view('main')->with('apelido',$value);
+    }
+
+    public function page2($value)
+    {
+        return view('page2', ['value' => $value]);
+
+        // return view('main')->with('value',$value);
+        // return view('main')->with('apelido',$value);
+    }
+
+    public function page3($value)
+    {
+        return view('page3', ['value' => $value]);
 
         // return view('main')->with('value',$value);
         // return view('main')->with('apelido',$value);
