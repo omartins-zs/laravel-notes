6582e16577a6169aeae3e02ef7c13cd21aa55ae1
omartins-zs <gabrielmatheus209@gmail.com>
2024-11-15 12:39:51 -0300

    Criando Rota para o MainController

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -11,3 +12,5 @@ Route::get('/about', function () {
     echo "About";
     // return view('welcome');
 });
+
+Route::get('/main', [MainController::class, 'index']);
