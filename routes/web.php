edb0d15227723350120dba7489ef4ee7d0211254
omartins-zs <gabrielmatheus209@gmail.com>
2024-11-15 12:22:40 -0300

    Fazendo Teste de Criação e Funcionamento das Rotas

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,11 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    echo "Hello World";
+    // return view('welcome');
+});
+
+Route::get('/about', function () {
+    echo "About";
+    // return view('welcome');
 });
