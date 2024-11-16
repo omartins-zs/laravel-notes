cbceb21d9dd252ea70256f98d3d35720d40cac38
omartins-zs <gabrielmatheus209@gmail.com>
2024-11-16 01:49:40 -0300

    Ajuste na Estrutura da Migration/Table de Users

--- database/migrations/2024_11_16_044153_create_users_table.php
+++ database/migrations/2024_11_16_044153_create_users_table.php
@@ -12,8 +12,12 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->id()->autoIncrement();
+            $table->string('username', 50)->nullable();
+            $table->string('password', 200)->nullable();
+            $table->dateTime('last_login')->nullable();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
