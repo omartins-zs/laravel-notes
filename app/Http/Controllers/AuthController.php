e7dccf9408f73948ed214ff11a845ba4147d21c3
omartins-zs <gabrielmatheus209@gmail.com>
2024-11-16 01:22:04 -0300

    Add Mensagem personalizada nos Erros de Validação

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -14,11 +14,21 @@ class AuthController extends Controller
     public function loginSubmit(Request $request)
     {
         // Form validation
-
-        $request->validate([
-            'text_username' => 'required|min:3',
-            'text_password' => ['required', 'min:3']
-        ]);
+        $request->validate(
+            // Rules
+            [
+                'text_username' => 'required|email',
+                'text_password' => ['required', 'min:6', 'max:16']
+            ],
+            // Error messages
+            [
+                'text_username.required' => 'O username é obrigatório',
+                'text_username.email' => 'Username deve ser um email válido',
+                'text_password.required' => 'A password é obrigatória',
+                'text_password.min' => 'A password deve ter pelo menos :min caracteres',
+                'text_password.max' => 'A password deve ter no máximo :max caracteres'
+            ]
+        );
 
         // Get user Input
         $username =  $request->input('text_username');
