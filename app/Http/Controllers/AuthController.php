cdc6ea5f76dc20bd454a3b8277a9f15089b857d4
omartins-zs <gabrielmatheus209@gmail.com>
2024-11-16 01:06:52 -0300

    Add Validação de Erros

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -13,7 +13,18 @@ class AuthController extends Controller
     }
     public function loginSubmit(Request $request)
     {
+        // Form validation
 
+        $request->validate([
+            'text_username' => 'required|min:3',
+            'text_password' => ['required', 'min:3']
+        ]);
+
+        // Get user Input
+        $username =  $request->input('text_username');
+        $password = $request->input('text_password');
+
+        echo "OK";
         dump($request);
 
         echo $request->input('text_username');
