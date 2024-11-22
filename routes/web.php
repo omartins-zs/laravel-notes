14052df4af52e8f45c8a7ac975544f4cfdbe0428
omartins-zs <gabrielmatheus209@gmail.com>
2024-11-22 11:11:15 -0300

    Criando Rota para Editar e Deletar Nota

--- routes/web.php
+++ routes/web.php
@@ -8,13 +8,20 @@ use Illuminate\Support\Facades\Route;
 
 // Auth routes - User not Logged
 Route::middleware([CheckIsNotLogged::class])->group(function () {
-Route::get('/login', [AuthController::class, 'login']);
-Route::post('/loginSubmit', [AuthController::class, 'loginSubmit']);
+    Route::get('/login', [AuthController::class, 'login']);
+    Route::post('/loginSubmit', [AuthController::class, 'loginSubmit']);
 });
 
 // App routes - User Logged
 Route::middleware([CheckIsLogged::class])->group(function () {
     Route::get('/', [MainController::class, 'index'])->name('home');
     Route::get('/newNote', [MainController::class, 'newNote'])->name('new');
+
+    // Edit note
+    Route::get('/editNote/{id}', [MainController::class, 'editNote'])->name('edit');
+
+    // Delete note
+    Route::get('/deleteNote/{id}', [MainController::class, 'deleteNote'])->name('delete');
+
     Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
 });
