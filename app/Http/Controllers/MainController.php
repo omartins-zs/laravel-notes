c5bab00885fa6922b77cb2fdb3f75259139b688e
omartins-zs <gabrielmatheus209@gmail.com>
2024-11-22 11:30:00 -0300

    Capturando e Validando IDs Encriptados Edit,Delete

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Illuminate\Contracts\Encryption\DecryptException;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Crypt;
 
 class MainController extends Controller
 {
@@ -22,5 +24,27 @@ class MainController extends Controller
     public function newNote()
     {
         echo "New note";
+    }
+    public function editNote($id)
+    {
+        try {
+            $id = Crypt::decrypt($id);
+            // $id =
+        } catch (DecryptException $e) {
+            return redirect()->route('home');
+        }
+
+        echo "Editando a nota ID:  " . $id;
+    }
+    public function deleteNote($id)
+    {
+        try {
+            $id = Crypt::decrypt($id);
+            // $id =
+        } catch (DecryptException $e) {
+            return redirect()->route('home');
+        }
+
+        echo "Delete a nota ID:  " . $id;
     }
 }
