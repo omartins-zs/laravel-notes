e1ad3cccdb4364c77abc71f36359137c0259ff1a
omartins-zs <gabrielmatheus209@gmail.com>
2024-11-22 08:05:11 -0300

    Criando Function Index e NewNote

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -7,5 +7,12 @@ use Illuminate\Http\Request;
 class MainController extends Controller
 {
     //
-
+    public function index()
+    {
+        echo "Home page";
+    }
+    public function newNote()
+    {
+        echo "New note";
+    }
 }
