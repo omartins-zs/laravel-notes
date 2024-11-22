2ecb50da7b5e8062ea20ddf0906b15b7641c40a0
omartins-zs <gabrielmatheus209@gmail.com>
2024-11-22 16:50:54 -0300

    Criando a Rota EditNoteSubmit

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,7 @@ Route::middleware([CheckIsLogged::class])->group(function () {
 
     // Edit note
     Route::get('/editNote/{id}', [MainController::class, 'editNote'])->name('edit');
+    Route::get('/editNoteSubmit', [MainController::class, 'editNoteSubmit'])->name('editNoteSubmit');
 
     // Delete note
     Route::get('/deleteNote/{id}', [MainController::class, 'deleteNote'])->name('delete');
