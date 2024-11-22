5172ddb790bfb170e8fe1f57f8772d0083bc2d8c
omartins-zs <gabrielmatheus209@gmail.com>
2024-11-22 14:52:16 -0300

    Criando e NewNoteSubmit e Add as Validações

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Note;
 use App\Models\User;
 use App\Services\Operations;
 use Illuminate\Contracts\Encryption\DecryptException;
@@ -26,6 +27,28 @@ class MainController extends Controller
     {
         // Show new Note View
         return view('new_note');
+    }
+
+    public function newNoteSubmit(Request $request)
+    {
+        // validate request
+        $request->validate(
+            // rules
+            [
+                'text_title' => 'required|min:3|max:200',
+                'text_note' => 'required|min:3|max:3000'
+            ],
+            // error messages
+            [
+                'text_title.required' => 'O título é obrigatório',
+                'text_title.min' => 'O título deve ter pelo menos :min caracteres',
+                'text_title.max' => 'O título deve ter no máximo :max caracteres',
+                'text_note.required' => 'A nota é obrigatória',
+                'text_note.min' => 'A nota deve ter pelo menos :min caracteres',
+                'text_note.max' => 'A nota deve ter no máximo :max caracteres'
+            ]
+        );
+        echo "OK";
     }
 
     public function editNote($id)
