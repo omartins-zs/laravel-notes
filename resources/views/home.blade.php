004dfbe95bd30074fa5efc1d23b6bda5af233f27
omartins-zs <gabrielmatheus209@gmail.com>
2024-11-22 10:51:40 -0300

    Separando a Apresentação das Notas do User Logado

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -18,7 +18,6 @@
                         </div>
                     </div>
                 @else
-
                     <!-- notes are available -->
                     <div class="d-flex justify-content-end mb-3">
                         <a href="#" class="btn btn-secondary px-3">
@@ -27,32 +26,8 @@
                     </div>
 
                     @foreach ($notes as $note)
-
-                    <div class="row">
-                        <div class="col">
-                            <div class="card p-4">
-                                <div class="row">
-                                    <div class="col">
-                                        <h4 class="text-info">{{$note['title']}}</h4>
-                                        <small class="text-secondary"><span class="opacity-75 me-2">Created
-                                                at:</span><strong>{{$note['created_at']}}</strong></small>
-                                    </div>
-                                    <div class="col text-end">
-                                        <a href="#" class="btn btn-outline-secondary btn-sm mx-1"><i
-                                                class="fa-regular fa-pen-to-square"></i></a>
-                                        <a href="#" class="btn btn-outline-danger btn-sm mx-1"><i
-                                                class="fa-regular fa-trash-can"></i></a>
-                                    </div>
-                                </div>
-                                <hr>
-                                <p class="text-secondary">{{$note['text']}}
-                                </p>
-                            </div>
-                        </div>
-                    </div>
+                        @include('note')
                     @endforeach
-
-
                 @endif
             </div>
         </div>
