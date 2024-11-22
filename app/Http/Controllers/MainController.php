bd058ae9de953d7ea863f09c6e91bc4bd881fb13
omartins-zs <gabrielmatheus209@gmail.com>
2024-11-22 10:37:17 -0300

    Carregando as Notas do User Logado

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class MainController extends Controller
@@ -9,8 +10,22 @@ class MainController extends Controller
     //
     public function index()
     {
-        echo "Home page";
+        // Load Users notes
+        $id = session('user.id');
+        $user = User::find($id)->toArray();
+        $notes = User::find($id)->notes()->get()->toArray();
+
+        echo '<pre>';
+        print_r($user);
+        print_r($notes);
+
+        die();
+
+
+        // Show Home View
+        return view('home', compact('user', 'notes'));
     }
+
     public function newNote()
     {
         echo "New note";
