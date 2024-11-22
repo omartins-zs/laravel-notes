b9427bca78b2ee2909f6a6dffa09b94403998043
omartins-zs <gabrielmatheus209@gmail.com>
2024-11-22 08:04:55 -0300

    Criando Rota para newNote

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\Route;
 // Auth routes
 Route::get('/login', [AuthController::class, 'login']);
 Route::post('/loginSubmit', [AuthController::class, 'loginSubmit']);
-Route::get('/logout', [AuthController::class, 'logout']);
 
 Route::get('/', [MainController::class, 'index']);
+Route::get('/newNote', [MainController::class, 'newNote']);
+Route::get('/logout', [AuthController::class, 'logout']);
