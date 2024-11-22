7d98372044e7b7e81e00a5b6beedfb576dc75bf4
omartins-zs <gabrielmatheus209@gmail.com>
2024-11-22 14:51:13 -0300

    Add Show Error do Ttile e Text das Notas

--- resources/views/new_note.blade.php
+++ resources/views/new_note.blade.php
@@ -27,16 +27,25 @@
                             <div class="mb-3">
                                 <label class="form-label">Note Title</label>
                                 <input type="text" class="form-control bg-primary text-white" name="text_title">
+                                {{-- show error --}}
+                                @error('text_title')
+                                    <div class="text-danger">{{ $message }}</div>
+                                @enderror
                             </div>
                             <div class="mb-3">
                                 <label class="form-label">Note Text</label>
                                 <textarea class="form-control bg-primary text-white" name="text_note" rows="5"></textarea>
+                                {{-- show error --}}
+                                @error('text_note')
+                                    <div class="text-danger">{{ $message }}</div>
+                                @enderror
                             </div>
                         </div>
                     </div>
                     <div class="row mt-3">
                         <div class="col text-end">
-                            <a href="{{ route('home') }}" class="btn btn-primary px-5"><i class="fa-solid fa-ban me-2"></i>Cancel</a>
+                            <a href="{{ route('home') }}" class="btn btn-primary px-5"><i
+                                    class="fa-solid fa-ban me-2"></i>Cancel</a>
                             <button type="submit" class="btn btn-secondary px-5"><i
                                     class="fa-regular fa-circle-check me-2"></i>Save</button>
                         </div>
