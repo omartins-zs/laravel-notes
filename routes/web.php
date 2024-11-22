a80f97aa199d3deb42de989d8365f4e6a83371ce
omartins-zs <gabrielmatheus209@gmail.com>
2024-11-22 08:37:24 -0300

    Usando o Middleware de User not Logged

--- routes/web.php
+++ routes/web.php
@@ -3,12 +3,16 @@
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\MainController;
 use App\Http\Middleware\CheckIsLogged;
+use App\Http\Middleware\CheckIsNotLogged;
 use Illuminate\Support\Facades\Route;
 
-// Auth routes
+// Auth routes - User not Logged
+Route::middleware([CheckIsNotLogged::class])->group(function () {
 Route::get('/login', [AuthController::class, 'login']);
 Route::post('/loginSubmit', [AuthController::class, 'loginSubmit']);
+});
 
+// App routes - User Logged
 Route::middleware([CheckIsLogged::class])->group(function () {
     Route::get('/', [MainController::class, 'index']);
     Route::get('/newNote', [MainController::class, 'newNote']);
