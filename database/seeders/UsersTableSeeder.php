2d59e57dcb04d3932af1381fadfb1239eaea2e81
omartins-zs <gabrielmatheus209@gmail.com>
2024-11-21 23:37:07 -0300

    Usando Seeders para Criar Users na Table

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -2,8 +2,8 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class UsersTableSeeder extends Seeder
 {
@@ -13,5 +13,22 @@ class UsersTableSeeder extends Seeder
     public function run(): void
     {
         //
+        DB::table('users')->insert([
+            [
+                'username' => 'user1@gmail.com',
+                'password' => bcrypt('abc123456'),
+                'created_at' => date('Y-m-d H:i:s')
+            ],
+            [
+                'username' => 'user2@gmail.com',
+                'password' => bcrypt('abc123456'),
+                'created_at' => date('Y-m-d H:i:s')
+            ],
+            [
+                'username' => 'user3@gmail.com',
+                'password' => bcrypt('abc123456'),
+                'created_at' => date('Y-m-d H:i:s')
+            ],
+        ]);
     }
 }
