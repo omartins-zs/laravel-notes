d23e5e1d2a6adad63bc09a823929bce560af5da1
omartins-zs <gabrielmatheus209@gmail.com>
2025-03-13 21:54:09 -0300

    Ajustando o Build do Email pela View

--- app/Mail/TestEmail.php
+++ app/Mail/TestEmail.php
@@ -5,11 +5,11 @@ namespace App\Mail;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
-use Illuminate\Mail\Mailables\Content;
-use Illuminate\Mail\Mailables\Envelope;
+// use Illuminate\Mail\Mailables\Content;
+// use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 
-class TestEmail extends Mailable
+class TestEmail extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
 
@@ -22,32 +22,40 @@ class TestEmail extends Mailable
     }
 
     /**
+     * Build the message.
+     */
+    public function build()
+    {
+        return $this->subject('Test Email')->view('emails.test');
+    }
+
+    /**
      * Get the message envelope.
      */
-    public function envelope(): Envelope
-    {
-        return new Envelope(
-            subject: 'Test Email',
-        );
-    }
+    // public function envelope(): Envelope
+    // {
+    //     return new Envelope(
+    //         subject: 'Test Email',
+    //     );
+    // }
 
     /**
      * Get the message content definition.
      */
-    public function content(): Content
-    {
-        return new Content(
-            view: 'view.name',
-        );
-    }
+    // public function content(): Content
+    // {
+    //     return new Content(
+    //         view: 'view.name',
+    //     );
+    // }
 
     /**
      * Get the attachments for the message.
      *
      * @return array<int, \Illuminate\Mail\Mailables\Attachment>
      */
-    public function attachments(): array
-    {
-        return [];
-    }
+    // public function attachments(): array
+    // {
+    //     return [];
+    // }
 }
