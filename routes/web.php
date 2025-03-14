05ba7a7a3692d1550c03023288b00811122cdf55
omartins-zs <gabrielmatheus209@gmail.com>
2025-03-13 21:12:15 -0300

    Criando Rotas de Test Log e Test Querys

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,8 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\MainController;
 use App\Http\Middleware\CheckIsLogged;
 use App\Http\Middleware\CheckIsNotLogged;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Route;
 
 // Auth routes - User not Logged
@@ -27,4 +29,15 @@ Route::middleware([CheckIsLogged::class])->group(function () {
     Route::get('/deleteNoteConfirm/{id}', [MainController::class, 'deleteNoteConfirm'])->name('deleteConfirm');
 
     Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
+});
+
+// Routes accessible to both logged and non-logged users
+Route::get('/test-query', function () {
+    DB::table('users')->get();
+    return 'Query executada!';
+});
+
+Route::get('/test-log', function () {
+    Log::info('Log de teste para o Telescope');
+    return 'Log gerado!';
 });
