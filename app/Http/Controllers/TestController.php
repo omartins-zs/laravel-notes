8f1e506440c35067f816adb6eda0fd8d3fed79da
omartins-zs <gabrielmatheus209@gmail.com>
2025-03-13 21:52:44 -0300

    Ajustando o Controller para Disparar o Email

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Jobs\SendTestEmail;
 use Illuminate\Http\Request;
 
 class TestController extends Controller
 {
-    //
+    public function sendEmail()
+    {
+        // Enfileira o job para enviar um e-mail
+        SendTestEmail::dispatch('teste@example.com');
+
+        return "O Job foi enviado para a fila!";
+    }
 }
